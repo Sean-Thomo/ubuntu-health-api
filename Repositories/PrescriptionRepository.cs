6915e9bd044d5a3b2dc0c2cfbbd4da57d5ccdd6b
Siphesihle Thomo <seansthomo@gmail.com>
2025-03-16 21:02:28 +0200

    FEATURE: finish implementing PrescriptionRepository

--- Repositories/PrescriptionRepository.cs
+++ Repositories/PrescriptionRepository.cs
@@ -19,6 +19,31 @@ namespace Backend.Repositories
             return await _context.Prescription.ToListAsync();
         }
 
-        public async Task<Prescription> GetPrescrip
+        public async Task<Prescription> GetPrescriptionByIdAsync(int id)
+        {
+            return await _context.Prescription.FindAsync(id);
+        }
+
+        public async Task AddPrescriptionAsync(Prescription prescription)
+        {
+            await _context.Prescription.AddAsync(prescription);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePrescriptionAsync(int id)
+        {
+            var prescription = await _context.Prescription.FindAsync(id);
+            if (prescription != null)
+            {
+                _context.Prescription.Remove(prescription);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdatePrescriptionAsync(Prescription prescription)
+        {
+            _context.Prescription.Update(prescription);
+            await _context.SaveChangesAsync()
+        }
     }
 }
