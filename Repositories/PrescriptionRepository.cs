044f38fddd5e594cbdd093c304f2873ee0ea8da1
Siphesihle Thomo <seansthomo@gmail.com>
2025-03-18 11:33:56 +0200

    FIX: use correct definition for Prescriptions

--- Repositories/PrescriptionRepository.cs
+++ Repositories/PrescriptionRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Task;
 using Backend.Models;
+using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Repositories
@@ -16,33 +17,33 @@ namespace Backend.Repositories
 
         public async Task<IEnumerable<Prescription>> GetAllPrescriptionsAsync()
         {
-            return await _context.Prescription.ToListAsync();
+            return await _context.Prescriptions.ToListAsync();
         }
 
         public async Task<Prescription> GetPrescriptionByIdAsync(int id)
         {
-            return await _context.Prescription.FindAsync(id);
+            return await _context.Prescriptions.FindAsync(id);
         }
 
         public async Task AddPrescriptionAsync(Prescription prescription)
         {
-            await _context.Prescription.AddAsync(prescription);
+            await _context.Prescriptions.AddAsync(prescription);
             await _context.SaveChangesAsync();
         }
 
         public async Task DeletePrescriptionAsync(int id)
         {
-            var prescription = await _context.Prescription.FindAsync(id);
+            var prescription = await _context.Prescriptions.FindAsync(id);
             if (prescription != null)
             {
-                _context.Prescription.Remove(prescription);
+                _context.Prescriptions.Remove(prescription);
                 await _context.SaveChangesAsync();
             }
         }
 
         public async Task UpdatePrescriptionAsync(Prescription prescription)
         {
-            _context.Prescription.Update(prescription);
+            _context.Prescriptions.Update(prescription);
             await _context.SaveChangesAsync();
         }
     }
